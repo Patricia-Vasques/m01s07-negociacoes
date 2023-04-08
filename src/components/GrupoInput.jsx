849dd4fcf43985c92b52fd14abc754e74044f581
Patrícia Vasques <patriciahhhh@gmail.com>
2023-04-08 12:23:12 -0300

    Atividade-6

--- src/components/GrupoInput.jsx
+++ src/components/GrupoInput.jsx
@@ -1,12 +1,22 @@
+import PropTypes from "prop-types";
 import React from "react"
 
-const GrupoInput = ({label, type, id,min, step, value, required}) => {
+export const GrupoInput =(props) => {
+    const {tipo, etiqueta, id, min, step, valor, aoDigitar } = props;
     return(
-        <div className="form-group">
-            <label htmlFor={id} className="form-grou">{label}</label>
-            <input type={type} id={id} className="form-control" min={min} step={step} value={value} required={required}/>
+        <div className="form-group col">
+            <label htmlFor={id}>{etiqueta}</label>
+            <input type={tipo} id={id} className="form-control" min={min} step={step} value={valor} required onChange={aoDigitar} />
         </div>
     )
 }
 
-export default GrupoInput;
+GrupoInput.propTypes = {
+    tipo: PropTypes.string.isRequired,
+    etiqueta: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    min: PropTypes.number,
+    step: PropTypes.number,
+    valor: PropTypes.string.isRequired,
+    aoDigitar: PropTypes.string.isRequired,
+};
