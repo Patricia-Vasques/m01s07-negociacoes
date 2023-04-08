a23bb650ba888dd1e097575bd8e145c86dfccb5c
Patrícia Vasques <patriciahhhh@gmail.com>
2023-04-08 12:21:19 -0300

    Arrumando o nome e atividade-5Ok

---
+++ src/components/Tabela.jsx
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+
+export const Tabela = ({ negociacoes }) => {
+  function totalizador() {
+    const valorTotal = negociacoes.reduce((totalizador, negociacao) => {
+      return (
+        totalizador + Number(negociacao.valor) * Number(negociacao.quantidade)
+      );
+    }, 0);
+
+    return valorTotal.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+  }
+  
+    return (
+    <div class="my-5">
+      <table class="table table-hover table-bordered">
+        <thead class="table-primary">
+          <tr>
+            <th>DATA</th>
+            <th>QUANTIDADE</th>
+            <th>VALOR</th>
+            <th>VOLUME</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+    )
+}
+
+Tabela.propTypes = {
+  negociacoes: PropTypes.array.isRequired,
+};
