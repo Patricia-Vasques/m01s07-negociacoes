9cdb877409e69439a4052f9691849c096e0f29e7
Patrícia Vasques <patriciahhhh@gmail.com>
2023-04-08 12:28:37 -0300

    Atividade-8

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react"
 
 import { Header } from "./components/Header";
 import { Formulario } from "./components/Formulario";
 import { Tabela } from "./components/Tabela"; 
 
 export const App = () => {
+  const [negociacoes, setNegociacoes] = useState({});
+
+  function adicionarNegociacao(negociacao) {
+    setNegociacoes([...negociacoes, negociacao]);
+  }
 
   return (
     <div>
